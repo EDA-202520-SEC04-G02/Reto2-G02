51dfdfcae1168fd8053042f2c9b48c27dc2cd82c
LuigiToby <la.rodrigueza12@uniandes.edu.co>
2025-10-01 22:00:38 -0500

    main con modificaciones importantes

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 import App.view as view
 
+# Solucionar problemas de buffer
+import csv
+csv.field_size_limit(2147483647)
+# Limite que la guía recomienda cambiar
+import sys
+default_limit = 1000
+sys.setrecursionlimit(default_limit*10)
+#-------------------------------------------------
 
 # Main function
-def main():
-    view.main()
+def main(): 
+    view.main() # La función main llama a main de view 
 
 
 # Main function call to run the program
